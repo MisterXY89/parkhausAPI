2b7460da8c4bbd3cb4d6c68d2eb167d661b604de
Tilman Kerl <tilman.kerl@uni-konstanz.de>
2020-08-01 18:12:19 +0200

    docstring parkhaus

--- parkhausAPI/parkhaus.py
+++ parkhausAPI/parkhaus.py
@@ -1,7 +1,34 @@
 
 
 class Parkhaus:
-    """docstring for Parkhaus."""
+    """
+    Class used to represent information about a car park
+
+    Attributes
+    ----------
+    entry : str
+        how and/or where to enter the car park
+    entry : str
+        how and/or where to enter the car park
+    openingHours : str
+        opening hours of the car park
+    hightLimit : str
+        empty if none / is a string in the format: x,yz m with x,y,z number
+    address : str
+        street, housenr and postal code
+    operator : str
+        address and name of the car house operator
+    spots : int
+        total number of available spots
+    freeSpots : int
+        free spots
+    occupiedSpots : int
+        occupied spots
+    tendency : str
+        tendency of free spots development: gleichbleibend, steigend or sinkend
+    image : str
+        an absolute image url of (the entry of) the car park
+    """
 
     def __init__(self, entry, openingHours, hightLimit, address, operator, spots, freeSpots, occupiedSpots, tendency, image):
         self.entry = entry
