9c4bd6c8e38e2fbb074ae3344b2fbf4db0c4468f
Tilman Kerl <tilman.kerl@uni-konstanz.de>
2020-08-01 17:55:18 +0200

    doc for api interface

--- parkhausAPI/api.py
+++ parkhausAPI/api.py
@@ -2,12 +2,39 @@
 from .wrapper import *
 
 class API:
-    """docstring for API."""
+    """
+    Interface for the ParkhausWrapper
+
+    Attributes
+    ----------
+    wrapper : ParkhausWrapper
+        used to perform the tasks
+        -> see wrapper.py for detailed doc
+    """
 
     def __init__(self):
         self.wrapper = ParkhausWrapper()
 
     def getInfo(self, name, spots=True, content=True):
+        """
+        Parameters
+        ----------
+        name : str
+            name of the car park
+        | maybe:
+        name : int
+            code of the car park
+        | maybe:
+        spots : bool
+            parse spots?
+        | maybe:
+        content : bool
+            parse content?
+
+        Return
+        ----------
+        parkhaus object with all infos
+        """
         if not spots and content or spots and not content:
             obj = self.wrapper.getInfo(name, spots=spots, content=content)
         else:
@@ -15,4 +42,11 @@ class API:
         return obj
 
     def getPlaces(self):
+        """
+        Getter
+
+        Return
+        ----------
+        list, places
+        """
         return self.wrapper.getPlaces()
